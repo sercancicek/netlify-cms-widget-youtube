241d73148271e234a77f3269846696680d4e13ef
sercanatx <sercan@atxanka.io>
2019-09-05 11:38:43 +0300

    44

--- src/Control.js
+++ src/Control.js
@@ -198,8 +198,8 @@ export default class Control extends React.Component {
 		const APIKey = this.props.field.get("APIkey");
 		const { data } = this.state;
 		console.log({ data });
-		const srcImgUrl = data.thumbnails && data.thumbnails.maxres ?
-			data.thumbnails.maxres.url : data.thumbnails.default.url
+		const srcImgUrl = data.thumbnails && (data.thumbnails.maxres ?
+			data.thumbnails.maxres.url : data.thumbnails.default.url)
 		return (
 			<div id={forID} className={classNameWrapper}>
 				{
